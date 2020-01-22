2da8745a09484ce7ab4070e8f483c82e913e015f
Natalia Villegas <villegasnaty@gmail.com>
2020-01-22 16:36:26 -0300

    adding swagger doc

---
+++ README.md
@@ -0,0 +1,17 @@
+Github: Find project in https://github.com/villegasnaty/icchallenge
+
+Tool version: Mysql: 5.7.27 Java: 8 SpringBoot: 2.2.3
+
+
+Colleción de postman con request:
+https://www.getpostman.com/collections/305cb80ebb5aaada100c
+
+El proyecto está deployado en aws con Elastic Beanstalk:
+
+Swagger doc: http://challenge-dev.us-east-1.elasticbeanstalk.com/swagger-ui.html
+
+Endpoints: 
+
+http://challenge-dev.us-east-1.elasticbeanstalk.com/listclientes
+http://challenge-dev.us-east-1.elasticbeanstalk.com/creacliente
+http://challenge-dev.us-east-1.elasticbeanstalk.com/kpideclientes
--- src/main/java/com/intercorp/challenge/model/Cliente.java
+++ src/main/java/com/intercorp/challenge/model/Cliente.java
@@ -2,6 +2,8 @@ package com.intercorp.challenge.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,13 +13,17 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@ApiModel("Model Cliente")
 public class Cliente {
     @NotEmpty(message = "Nombre no puede estar vacío")
+    @ApiModelProperty(value = "Nombre del cliente", required = true)
     private String nombre;
     @NotEmpty(message = "Apellido no puede estar vacío")
+    @ApiModelProperty(value = "Apellido del cliente", required = true)
     private String apellido;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Integer edad;
+    @ApiModelProperty(value = "Fecha de nacimiento del cliente", required = true)
     @NotNull(message = "fecha de nacimiento no puede estar vacío")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "GMT-3")
     private LocalDate fechaDeNacimiento;
